21dd07410baaf1fe2104ed4dac2b8e8c46cf318e
abhishekchetani <abhishekchetani@gmail.com>
2021-08-10 13:40:52 +0530

    Added comments for better understandability of code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Selecting The Score To Be Displayed / Updated
+// Selecting The Score To Be Displayed / Updated using querySelector
 const p1ScoreBoard = document.querySelector('#p1Score');
 const p2ScoreBoard = document.querySelector('#p2Score');
 
@@ -9,6 +9,7 @@ const resetButton = document.querySelector('#reset');
 
 // Selecting The Winning Game Point 'select' Tag
 const winningGamePoint = document.querySelector('#gamePoint');
+// Checking the Select value in Console
 // console.log(winningGamePoint.value)
 
 // Initializing The Scores To Be 0
@@ -16,9 +17,10 @@ let p1Score = 0;
 let p2Score = 0;
 // Player With Threshold Game Point Will Win
 let winningScore = parseInt(winningGamePoint.value);
+// Starting the Game
 let isGameOver = false;
 
-// Listening For Clicks On Player 1 Button
+// Listening For Click Event On Player 1 Button
 p1Button.addEventListener('click', (evt) => {
     // console.log(evt);
     // Playing Game & Updating The Score Till A Player Wins
@@ -26,13 +28,15 @@ p1Button.addEventListener('click', (evt) => {
         p1Score += 1;
         p1ScoreBoard.innerText = p1Score;
     }
+
     // Setting The isGameOver Value To 'true' & Giving Color Classes
     if (p1Score === winningScore) {
         isGameOver = true;
-        // p1ScoreBoard.classList.add('winner');
-        // p2ScoreBoard.classList.add('loser');
-        p1ScoreBoard.classList.add('has-text-success');     //Using BULMA Framework Color Classes
-        p2ScoreBoard.classList.add('has-text-danger');
+        p1ScoreBoard.classList.add('winner');
+        p2ScoreBoard.classList.add('loser');
+        // p1ScoreBoard.classList.add('has-text-success');     //Using BULMA Framework Color Classes
+        // p2ScoreBoard.classList.add('has-text-danger');
+
         // Disabling The Buttons
         p1Button.disabled = true;
         p2Button.disabled = true;
@@ -49,10 +53,11 @@ p2Button.addEventListener('click', (evt) => {
     // Defining Color Classes For Winner & Loser
     if (p2Score === winningScore) {
         isGameOver = true;
-        // p2ScoreBoard.classList.add('winner');
-        // p1ScoreBoard.classList.add('loser');
-        p2ScoreBoard.classList.add('has-text-success');
-        p1ScoreBoard.classList.add('has-text-danger');
+        p2ScoreBoard.classList.add('winner');
+        p1ScoreBoard.classList.add('loser');
+        // p2ScoreBoard.classList.add('has-text-success');
+        // p1ScoreBoard.classList.add('has-text-danger');
+
         // Disabling The Buttons
         p1Button.disabled = true;
         p2Button.disabled = true;
@@ -66,10 +71,10 @@ function resetGame() {
     p1ScoreBoard.innerText = 0;
     p2ScoreBoard.innerText = 0;
     isGameOver = false;
-    // p1ScoreBoard.classList.remove('winner', 'loser');
-    // p2ScoreBoard.classList.remove('winner', 'loser');
-    p1ScoreBoard.classList.remove('has-text-success', 'has-text-danger');
-    p2ScoreBoard.classList.remove('has-text-success', 'has-text-danger');
+    p1ScoreBoard.classList.remove('winner', 'loser');
+    p2ScoreBoard.classList.remove('winner', 'loser');
+    // p1ScoreBoard.classList.remove('has-text-success', 'has-text-danger');
+    // p2ScoreBoard.classList.remove('has-text-success', 'has-text-danger');
     p1Button.disabled = false;
     p2Button.disabled = false;
 }
@@ -78,7 +83,7 @@ function resetGame() {
 resetButton.addEventListener('click', resetGame)
 
 // Setting The 'winningScore' Value To The Selected Value & Resetting Game If It Is Changed
-winningGamePoint.addEventListener('change', () => {
+winningGamePoint.addEventListener('change', (evt) => {
     winningScore = parseInt(winningGamePoint.value);
     resetGame();
 })
